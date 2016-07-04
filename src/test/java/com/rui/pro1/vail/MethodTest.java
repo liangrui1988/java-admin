ebb1a1830dca416e3c9b91670e3b360d587d1703
ruiliang <liangrui_1988@126.com>
2016-07-05 01:57:21 +0800

    顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶顶

--- src/test/java/com/rui/pro1/vail/MethodTest.java
+++ src/test/java/com/rui/pro1/vail/MethodTest.java
@@ -1,16 +1,9 @@
 package com.rui.pro1.vail;
 
-import static org.junit.Assert.assertEquals;
-
 import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
-import javax.validation.Path.MethodNode;
-import javax.validation.Path.Node;
-import javax.validation.Path.ParameterNode;
 import javax.validation.Validation;
 import javax.validation.ValidatorFactory;
 import javax.validation.executable.ExecutableValidator;
@@ -33,21 +26,52 @@ public class MethodTest {
 	}
 
 	@Test
-	public void methodT() throws NoSuchMethodException, SecurityException {
+	public void methodT() throws NoSuchMethodException, SecurityException,
+			InstantiationException, IllegalAccessException {
 
-		Car object = new Car("Morris");
-		Method method = Car.class.getMethod("drive", int.class, String.class);
+		// Car object = new Car("Morris");
+
+		Class clz = Car.class;
+
+		Object object = clz.newInstance();
+
+		Method method = Car.class.getMethod("drive", int.class, String.class,
+				String.class, String.class, BeanVail.class);
 
 		// Object[] parameterValues = { 1,null};
-		Object[] parameterValues = { 1, "a" };
+		BeanVail b = new BeanVail();
+		b.setAge(10000);
+		Object[] parameterValues = { 1, "a", "", "", b };
 		// Object[] parameterValues = { 100,"abcdef"};
-		Set<ConstraintViolation<Car>> violations = executableValidator
+		Set<ConstraintViolation<Object>> violations = executableValidator
 				.validateParameters(object, method, parameterValues);
 
 		System.out.println(violations);
 		for (ConstraintViolation cv : violations) {
 			System.out.println(cv.getMessage());
 		}
+
+	}
+
+	@Test
+	public void pp() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException {
+		Class clzcar = Car.class;
+
+		Object objectcar = clzcar.newInstance();
+		Method methodx = Car.class.getMethod("drive", int.class, String.class,
+				String.class, String.class, BeanVail.class);
+
+		BeanVail b = new BeanVail();
+		b.setAge(10000);
+		Object[] parameterValuestest = { 1, "a", "", "", b };
+		// Object[] parameterValues = { 100,"abcdef"};
+		Set<ConstraintViolation<Object>> violations = executableValidator.validateParameters(objectcar, methodx,
+						parameterValuestest);
+		System.out.println(violations);
+		for (ConstraintViolation cv : violations) {
+			System.out.println(cv.getMessage());
+		}
+
 
 	}
 }
--- src/test/java/com/rui/pro1/vail/v/Car.java
+++ src/test/java/com/rui/pro1/vail/v/Car.java
@@ -1,8 +1,11 @@
 package com.rui.pro1.vail.v;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.rui.pro1.vail.BeanVail;
 
 public class Car {
 
@@ -16,6 +19,11 @@ public class Car {
 	@Min(2)
 	private int seatCount;
 
+	
+	public Car() {
+		
+	}
+	
 	public Car(String manufacturer, String licencePlate, int seatCount) {
 		this.manufacturer = manufacturer;
 		this.licensePlate = licencePlate;
@@ -24,12 +32,13 @@ public class Car {
 	
 	public void drive(@Min(2)int driver) { 
 		
+		Object [] s;
 		System.out.println("driver:"+driver);
 		this.seatCount=driver;
 	}
 	
 	
-	public void drive(@Min(2)int driver,@NotNull @Size(min = 2, max = 14) String name ) { 
+	public void drive(@Min(2)int driver,@NotNull @Size(min = 2, max = 14) String name ,String a,String b,@Valid BeanVail vail) { 
 		System.out.println("driver:"+driver);
 		this.seatCount=driver;
 	}
